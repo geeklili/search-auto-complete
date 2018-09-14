e691dc0c98bdfbf96c41b0876e42014d1279ac18
unknown <shicheng.li@pashr.com>
2018-09-14 16:16:50 +0800

    pure file

--- n01_create_pure_idioms.py
+++ n01_create_pure_idioms.py
@@ -1,11 +1,18 @@
 import re
 
-with open('./data/origin_data.txt', encoding='utf-8') as f:
-    with open('./data/idioms.txt', 'w', encoding='utf-8') as f2:
-        for i in f:
-            try:
-                a = re.findall('（.*?）', i)[0].replace('）', '').replace('（', '')
-                print(a)
-                f2.write(a+'\n')
-            except:
-                pass
+
+def create_pure():
+    """创建需求的成语文件"""
+    with open('./data/origin_data.txt', encoding='utf-8') as f:
+        with open('./data/idioms.txt', 'w', encoding='utf-8') as f2:
+            for i in f:
+                try:
+                    a = re.findall('（.*?）', i)[0].replace('）', '').replace('（', '')
+                    print(a)
+                    f2.write(a + '\n')
+                except:
+                    pass
+
+
+if __name__ == '__main__':
+    create_pure()
